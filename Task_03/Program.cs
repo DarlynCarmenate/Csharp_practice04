022e0ff460e7ee5f9e8054240a25ed9e424fdad0
darlyncarmenate <darlyncarmenate@gmail.com>
2022-03-29 18:13:34 +0200

    Функции на заполнение и вывод массива

---
+++ Task_03/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов случайными числами 
+// и выводит их на экран. 
+// Оформите заполнение массива и вывод в виде функции (пригодится в следующих задачах)
+
+void FindArray(int[] arr)
+{
+	Random rnd = new Random();
+	for(int i = 0; i < arr.Length; i++)
+	{
+		arr[i] = rnd.Next(0, 100);
+	}
+}
+
+void PrintArray(int[] coll)
+{
+	Console.Write("[");
+	for(int i = 0; i < coll.Length - 1; i++)
+	{
+		Console.Write(coll[i] + ",");
+	}
+	Console.Write(coll[7] + "]");
+}
+
+int[] arr = new int[8];
+FindArray(arr);
+PrintArray(arr);
