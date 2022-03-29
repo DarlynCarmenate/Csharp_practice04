dc303d2bab7e6b54c90fcef40bdac001fe7f7943
darlyncarmenate <darlyncarmenate@gmail.com>
2022-03-29 17:57:44 +0200

    Сумма цифр от 1 до N

---
+++ Task_02/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int Prompt(string message)
+{
+	Console.Write(message);
+	string strValue = Console.ReadLine();
+	int value = int.Parse (strValue);
+	return value;
+}
+
+int Summ(int value)
+{
+	int sum = 0;
+	for(int i = 1; i <= value; i++)
+	{
+		sum = sum + i;
+	}
+	return sum;
+}
+
+int number = Prompt("Введите число:");
+int result = Summ(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} равно {result}");
