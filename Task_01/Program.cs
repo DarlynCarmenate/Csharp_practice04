fc35a7bb574ca4797c9c5bd8b415a47a14924c4f
darlyncarmenate <darlyncarmenate@gmail.com>
2022-03-29 17:19:07 +0200

    Возводим число А в степень B

---
+++ Task_01/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+
+int A = Prompt("Введите число А: ");
+int B = Prompt("Введите число B: ");
+int value = Degree(A, B);
+Console.WriteLine($"Число {A} в степени {B} равняется {value}");
+
+int Degree(int num1, int num2)
+{
+    int result = num1;
+    for (int i = 1; i < num2; i++)
+    {
+        result = result * num1;
+    }
+    return result;
+}
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int value = int.Parse(strValue);
+    return value;
+}
