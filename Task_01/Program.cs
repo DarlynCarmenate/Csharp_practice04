b100fb71d28c06ef4facee9662c3ca420848b169
darlyncarmenate <darlyncarmenate@gmail.com>
2022-03-29 17:40:09 +0200

    Возведение числа А в степень B

--- Task_01/Program.cs
+++ Task_01/Program.cs
@@ -3,8 +3,22 @@
 
 int A = Prompt("Введите число А: ");
 int B = Prompt("Введите число B: ");
+
+if(A < 0|| B < 0)
+    {
+    Console.WriteLine("Вы ввели ненатуральное число");
+    }
+
 int value = Degree(A, B);
 Console.WriteLine($"Число {A} в степени {B} равняется {value}");
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int value = int.Parse(strValue);
+    return value;
+}
 
 int Degree(int num1, int num2)
 {
@@ -16,10 +30,3 @@ int Degree(int num1, int num2)
     return result;
 }
 
-int Prompt(string message)
-{
-    Console.Write(message);
-    string strValue = Console.ReadLine();
-    int value = int.Parse(strValue);
-    return value;
-}
